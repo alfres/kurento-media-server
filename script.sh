a5144584d39a116b1138e5e62731c657423c5058
alfres <alfre_alfree@yahoo.es>
2017-06-06 19:59:39 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+  echo "deb http://ubuntu.kurento.org trusty-dev kms6" | sudo tee /etc/apt/sources.list.d/kurento-dev.list
+  wget -O - http://ubuntu.kurento.org/kurento.gpg.key | sudo apt-key add -
+  sudo apt-get update
+  sudo apt-get install $(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\! / /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends: //g" | sed "s/([^)]*)//g" | sed "s/, */ /g")
+  mkdir -p build
+  cd build
+  cmake ..
+  make -j4
+sudo service kurento-media-server start
+exit
